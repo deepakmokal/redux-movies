ab59020244f832bd6227e6970665ea1fb1f30dce
deepakmokal <deepak.mokal@think.design>
2023-01-17 12:06:40 +0530

    Pag Not Found Page

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -29,7 +29,7 @@ export default function MovieDetails() {
             <>
            
             <div className='section-left'>
-            <div className='movie-title'>{data.Title}</div>
+             <div className='movie-title'>{data.Title}</div>
                 <div className='movie-rating'>
                     <span>
                         IMDB Rating <i className='fas fa-star'></i> : {data.imdbRating} 
--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import './PageNotFound.scss';
+import { Link } from 'react-router-dom';
 
 export default function PageNotFound() {
     return (
         <div>
-            PageNotFound
+            <div class="flex-container">
+                <div class="text-center">
+                    <h1>
+                        <span class="fade-in" id="digit1">4</span>
+                        <span class="fade-in" id="digit2">0</span>
+                        <span class="fade-in" id="digit3">4</span>
+                    </h1>
+                    <h3 class="fadeIn">PAGE NOT FOUND</h3>
+                    <a className='return-home' >
+                    <Link to="/">
+                        Return to Home
+                    </Link>
+            </a>
+                </div>
+            </div>
         </div>
     )
 }
